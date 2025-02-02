f78da97131595b967ffb8b82c4a6e8c95130296b
jimmyfl0 <jflorence0@gmail.com>
2025-02-02 15:41:10 -0500

    Initial Commit

---
+++ AgoraTests/AgoraTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraTests.swift
+//  AgoraTests
+//
+//  Created by James Florence on 2/2/25.
+//
+
+import Testing
+@testable import Agora
+
+struct AgoraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
